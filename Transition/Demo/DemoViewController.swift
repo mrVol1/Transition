a1882c9c88751ab9e5b13fed4e74852003868ca2
Eduard <eduardkarimov.rb@gmail.com>
2023-05-24 20:28:40 +0500

    new app - new commit

---
+++ Transition/Demo/DemoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DemoViewController.swift
+//  Transition
+//
+//  Created by Eduard Karimov on 24.05.2023.
+//
+
+import UIKit
+
+class demoViewController: UIViewController {
+    
+    @IBAction func backToLoginButton() {
+    }
+    
+}
--- Transition/Main/ViewController.swift
+++ Transition/Main/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func demoModeOpenButton() {
+        
+    }
+    
+    @IBAction func loginButton() {
+        
+    }
+    
+    @IBAction func termsButton() {
+        
+    }
 }
 
